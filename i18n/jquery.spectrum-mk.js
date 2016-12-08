79c08f5037a403b4a4297a7cefe3cc42743b130e
borache <mondo@mondo.mk>
2016-12-08 10:54:03 +0100

    Create jquery.spectrum-mk.js

---
+++ i18n/jquery.spectrum-mk.js
@@ -0,0 +1,18 @@
+// Spectrum Colorpicker
+// Macedonian (mk) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["hr"] = {
+        cancelText: "Одкажи",
+        chooseText: "Избери",
+        clearText: "Поништи избор",
+        noColorSelectedText: "Ниту една боја не е избрана",
+        togglePaletteMoreText: "повеќе",
+        togglePaletteLessText: "помалку"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
